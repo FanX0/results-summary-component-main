10d998ff2a12d29f39a71d3abfd25c27c533498a
fanx <faridazhari46@gmail.com>
2025-10-10 22:26:59 +0700

    fix height card

--- src/components/ui/card/Card.tsx
+++ src/components/ui/card/Card.tsx
@@ -4,7 +4,7 @@ import CardSummary from "./CardSummary";
 
 const Card = ({ data }: CardProps) => {
   return (
-    <div className="w-full bg-white grid grid-cols-1 gap-[1.5rem] lg:grid-cols-2 lg:w-[44rem] lg:h-[25rem] lg:rounded-4xl lg:shadow-xl/50 shadow-blue-500/30">
+    <div className="w-full bg-white grid grid-cols-1 gap-[1.5rem] lg:grid-cols-2 lg:w-[44rem] lg:h-[26rem] lg:rounded-4xl lg:shadow-xl/50 shadow-blue-500/30 ">
       <CardResult />
       <CardSummary data={data} />
     </div>
