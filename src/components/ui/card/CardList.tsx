1a4f693261f5d497b26c0b27dd87521de396b93c
fanx <faridazhari46@gmail.com>
2025-10-10 22:43:20 +0700

    fix bg card list

--- src/components/ui/card/CardList.tsx
+++ src/components/ui/card/CardList.tsx
@@ -6,21 +6,21 @@ interface CardListProps {
 }
 
 const CardList = ({ data, className }: CardListProps) => {
-  const COLOR: Record<string, { text: string }> = {
-    Reaction: { text: "text-Light-red" },
-    Memory: { text: "text-corangey-yellow" },
-    Verbal: { text: "text-Green-teal" },
-    Visual: { text: "text-Cobalt-blue" },
+  const COLOR: Record<string, { text: string; bg: string  }> = {
+    Reaction: { text: "text-Light-red", bg: "bg-Light-red/10" },
+    Memory: { text: "text-corangey-yellow", bg: "bg-corangey-yellow/10" },
+    Verbal: { text: "text-Green-teal", bg: "bg-Green-teal/10" },
+    Visual: { text: "text-Cobalt-blue", bg: "bg-Cobalt-blue/10" },
   };
 
   return (
     <div className={`flex flex-col gap-[1.5rem] lg:gap-[2rem] ${className ?? ""}`}>
       {data.map((d) => {
-        const c = COLOR[d.category] ?? { text: "text-black" };
+        const c = COLOR[d.category] ?? { text: "text-black", bg: "bg-transparent" };
         return (
-          <div key={d.category} className="flex justify-between">
+          <div key={d.category} className={`flex items-center px-[1rem] justify-between h-[3rem] rounded-xl ${c.bg}`}>
             <div className="flex gap-2">
-              <img src={d.icon} alt={d.category} />
+              <img src={d.icon} alt={d.category} className="w-[1.5rem]" />
               <p className={c.text}>{d.category}</p>
             </div>
             <div className="flex  gap-[0.4rem]">
--- src/components/ui/card/CardSummary.tsx
+++ src/components/ui/card/CardSummary.tsx
@@ -6,7 +6,7 @@ const CardSummary = ({ data }: CardProps) => {
   return (
     <div className="flex flex-col gap-[1.5rem] px-[1.5rem] lg:py-[2rem] lg:gap-[2rem] ">
       <h2 className="font-bold text-Dark-gray-blue text-xl">Summary</h2>
-      <CardList className="mx-[1rem]" data={data} />
+      <CardList className="" data={data} />
       <CardButton />
     </div>
   );
